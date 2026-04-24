e21dd91adef8e78018dee0431bd31525f7a892b0
MiQ <kokobay2004@gmail.com>
2026-04-24 11:29:47 +0700

    fix_setup

---
+++ src/app/(main)/courses/[courseId]/lessons/[lessonId]/page.tsx
@@ -0,0 +1,7 @@
+export default function LessonDetailPage({
+  params,
+}: {
+  params: Promise<{ courseId: string; lessonId: string }>;
+}) {
+  return <div />;
+}
---
+++ src/app/(main)/sample-essays/[essayId]/page.tsx
@@ -0,0 +1,3 @@
+export default function SampleEssayDetailPage() {
+  return <div />;
+}
